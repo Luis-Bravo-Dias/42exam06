0b69292a1083fcfde0c54d70d2e52f55bdd919ea
Luis Dias <kazu@pop-os.localdomain>
2024-07-09 14:45:27 +0100

    9

--- 3/mini_serve.c
+++ 3/mini_serve.c
@@ -84,83 +84,91 @@ void	send_msg(int fd)
 }
 
 int main(int ac, char **av) {
+    if (ac != 2)
+        ft_error("Wrong number of arguments\n");
 
-	if (ac != 2)
-		ft_error("Wrong number of arguments\n");
-	int sockfd, connfd, cliId;
-	cliId = 0;
-	struct sockaddr_in servaddr, cliaddr;
-	bzero(&servaddr, sizeof(servaddr));
-	socklen_t len_cli = sizeof(cliaddr);
+    int sockfd, connfd, cliId = 0;
+    struct sockaddr_in servaddr, cliaddr;
+    bzero(&servaddr, sizeof(servaddr));
+    socklen_t len_cli = sizeof(cliaddr);
 
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); //127.0.0.1
-	servaddr.sin_port = htons(atoi(av[1]));
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); //127.0.0.1
+    servaddr.sin_port = htons(atoi(av[1]));
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-		ft_error("Fatal error\n");
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        ft_error("Fatal error\n");
 
-	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) < 0)
-		ft_error("Fatal error\n");
-	if (listen(sockfd, SOMAXCONN) < 0)
-		ft_error("Fatal error\n");
-	maxSock = sockfd;
-	FD_ZERO(&atv_set);
-	FD_SET(sockfd, &atv_set);
+    // Binding newly created socket to given IP and verification 
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+        ft_error("Fatal error\n");
+    if (listen(sockfd, SOMAXCONN) < 0)
+        ft_error("Fatal error\n");
+    maxSock = sockfd;
+    FD_ZERO(&atv_set);
+    FD_SET(sockfd, &atv_set);
 
-	while (1)
-	{
-		rd_set = wrt_set = atv_set;
-		if (select(maxSock + 1, &rd_set, &wrt_set, 0, 0) <= 0)
-			continue;
-		if (FD_ISSET(sockfd, &rd_set))
-		{
-			connfd = accept(sockfd, (struct sockaddr *)&cliaddr, &len_cli);
-			if (connfd < 0)
-				ft_error("Fatal error\n");
-			FD_SET(connfd, &atv_set);
-			maxSock = (connfd > maxSock) ? connfd : maxSock;
-			all_cliID[connfd] = cliId++;
-			sprintf(buff_send, "server: client %d just arrived\n", all_cliID[connfd]);
-			send_msg(connfd);
-			cliBuff[connfd] = 0;
-			continue;
-		}
-		for (int ID = 3; ID < maxSock; ID++)
-		{
-			if (FD_ISSET(ID, &rd_set) && ID != sockfd)
-			{
-				int	readeing = recv(ID,buff_read, 1000, 0);
-				if (readeing <= 0)
-				{
-					FD_CLR(ID, &atv_set);
-					sprintf(buff_send, "server: client %d just left\n", all_cliID[ID]);
-					send_msg(ID);
-					if (cliBuff[ID] != 0)
-						free(cliBuff[ID]);
-					close(ID);
-				}
-				else
-				{
-					buff_read[readeing] = 0;
-					cliBuff[ID] = str_join(cliBuff[ID], buff_read);
-					msg = 0;
-					while (extract_message(&cliBuff[ID], &msg))
-					{
-						sprintf(buff_send, "client %d: ", all_cliID[ID]);
-						send_msg(ID);
-						if (msg)
-						{
-							free(msg);
-							msg = 0;
-						}
-					}
-				}
-			}
-		}
-	}
-	return (0);
+    while (1)
+    {
+        fd_set rd_set, wrt_set;
+        rd_set = wrt_set = atv_set;
+
+        if (select(maxSock + 1, &rd_set, &wrt_set, 0, 0) <= 0)
+            continue;
+
+        if (FD_ISSET(sockfd, &rd_set))
+        {
+            connfd = accept(sockfd, (struct sockaddr *)&cliaddr, &len_cli);
+            if (connfd < 0)
+                ft_error("Fatal error\n");
+
+            FD_SET(connfd, &atv_set);
+            maxSock = (connfd > maxSock) ? connfd : maxSock;
+            all_cliID[connfd] = cliId++;
+            printf("server: client %d just arrived\n", all_cliID[connfd]);
+            fflush(stdout);
+            cliBuff[connfd] = NULL;
+            continue;
+        }
+
+        for (int ID = 3; ID <= maxSock; ID++)
+        {
+            if (FD_ISSET(ID, &rd_set) && ID != sockfd)
+            {
+                int reading = recv(ID, buff_read, sizeof(buff_read) - 1, 0);
+                if (reading <= 0)
+                {
+                    FD_CLR(ID, &atv_set);
+                    printf("server: client %d just left\n", all_cliID[ID]);
+                    fflush(stdout);
+                    send_msg(ID);
+                    if (cliBuff[ID] != NULL)
+                        free(cliBuff[ID]);
+                    close(ID);
+                }
+                else
+                {
+                    buff_read[reading] = '\0';
+                    cliBuff[ID] = str_join(cliBuff[ID], buff_read);
+                    char *msg = NULL;
+                    while (extract_message(&cliBuff[ID], &msg))
+                    {
+                        printf("client %d: %s\n", all_cliID[ID], msg);
+                        fflush(stdout);
+                        sprintf(buff_send, "client %d: %s\n", all_cliID[ID], msg);
+                        send_msg(ID);
+                        if (msg)
+                        {
+                            free(msg);
+                            msg = NULL;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
 }
